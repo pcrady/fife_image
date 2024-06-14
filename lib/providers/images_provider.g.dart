37c5508d75f6a29d94017077a25dad3482f6d7e1
Peter Crady <petercrady@pcrady-osx.local>
2024-06-14 17:32:37 -0500

    add file

---
+++ lib/providers/images_provider.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'images_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$imagesHash() => r'3715e3f372ae53f8163b5e3a9d8dbab2cfc76ab4';
+
+/// See also [Images].
+@ProviderFor(Images)
+final imagesProvider =
+    AutoDisposeAsyncNotifierProvider<Images, List<AbstractImage>>.internal(
+  Images.new,
+  name: r'imagesProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$imagesHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$Images = AutoDisposeAsyncNotifier<List<AbstractImage>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
