0fa0c0ad31381172d8d29c1fba5fb3ab2454c44f
Peter Crady <petercrady@pcrady-osx.local>
2024-06-16 09:56:31 -0500

    add file

---
+++ lib/providers/convex_hull_image_provider.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'convex_hull_image_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$convexHullImageProviderHash() =>
+    r'7345284d1f98e275a0efd12ef40a98dd39a2cc95';
+
+/// See also [ConvexHullImageProvider].
+@ProviderFor(ConvexHullImageProvider)
+final convexHullImageProviderProvider = AutoDisposeNotifierProvider<
+    ConvexHullImageProvider, Map<String, List<AbstractImage>>>.internal(
+  ConvexHullImageProvider.new,
+  name: r'convexHullImageProviderProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$convexHullImageProviderHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ConvexHullImageProvider
+    = AutoDisposeNotifier<Map<String, List<AbstractImage>>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
